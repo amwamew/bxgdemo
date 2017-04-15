91d7887edceda1067aabe4560162f5719f6ae13d
amwamew <amwamew@163.com>
2017-04-15 17:47:33 +0800

    实现退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -6,7 +6,7 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
-
+			//实现登录功能
 	$('#loginForm').submit(function(){
 		var formData = $(this).serialize();
 		//调用接口
@@ -16,8 +16,8 @@
 			data:formData,
 			dataType:'json',
 			success:function(data){
-				var tc_avater = data.result.tc_avater;
-				var tc_name = data.result.tc_name;
+				// var tc_avater = data.result.tc_avater;
+				// var tc_name = data.result.tc_name;
 
 				if(data.code == 200){
 					location.href = '/index/index';
@@ -28,4 +28,18 @@
 		//jquery中的return false既可以阻止默认行为，也可以阻止冒泡
 		//原生js种的return false只能阻止默认行为，不能阻止冒泡
 		return false;
+	});
+
+	//实现退出功能
+	$('#logout').click(function(){
+		$.ajax({
+			type:'post',
+			url:'/api/logout',
+			dataType:'json',
+			success:function(data){
+				if(data.code==200){
+					location.href='/login';
+				}
+			}
+		});
 	});
